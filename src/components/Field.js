4c7e47fe9a55881ca290e0c5e384ac06f0110cc7
FilipPietryga <filippatrykpietryga@gmail.com>
2022-11-12 23:19:44 +0100

    Field component extended

--- src/components/Field.js
+++ src/components/Field.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "../scss/types.scss"
 
 function Field (props) {
-  const {image, tag, name, types} = props
+  const {img, tag, name, type} = props
   return (
-    <div>
-      <img src={image}/>
+    //refactor: reduce so many styles in one line ???
+    <div style={{width: "160px"}} className="m-1 d-flex flex-column justify-content-center align-items-center border border-secondary rounded">
+      <img src={img} alt={name} className="FieldImage"/>
       <h5>{tag} {name}</h5>
-      {types.forEach((id, name) => {
-        <span class={name}>{name}</span>
-      })}
+      <div className="w-75 d-flex justify-content-around">
+        {type.map((name) => (
+          <span className={name + " border rounded"} key={name.length}>{name}</span>
+        ))} 
+      </div>
     </div>
   )
 }
